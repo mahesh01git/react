fa117ca474cecb426d647ab1fdfe9f9ff0967ee0
mahesh01git <151907025+mahesh01git@users.noreply.github.com>
2024-01-02 16:56:16 +0530

    Update index.js

--- src/components/Speedometer/index.js
+++ src/components/Speedometer/index.js
@@ -5,20 +5,36 @@ import './index.css'
 import {Component} from 'react'
 
 class Speedometer extends Component {
+  status = {count: 0}
+
+  onAccelerate = () => {
+    this.setState(prevState => ({count: prevState.count + 1}))
+  }
+
+  onBreaks = () => {
+    this.setState(prevState => ({count: prevState.count - 1}))
+  }
+
   render() {
+    const {count} = this.status
     return (
       <div className="bg-container">
         <div className="content-con">
-          <h1>SPEEDOMETER</h1>
+          <h1 className="heading">SPEEDOMETER</h1>
           <img
-            src="https://assets.ccbp.in/frontend/react-js/speedometer-img.png alt should be speedometer"
+            className="img"
+            src="https://assets.ccbp.in/frontend/react-js/speedometer-img.png"
             alt="speedometer"
           />
-          <h>speed is {0}mph</h>
-          <p>min limit is 0mph, max limit is 200mph</p>
+          <h1 className="speed">speed is {count}mph</h1>
+          <p className="para">min limit is 0mph, max limit is 200mph</p>
           <div>
-            <button type="button">Accelerate</button>
-            <button type="button">Apply breaks</button>
+            <button type="button" onClick={this.onAccelerate}>
+              Accelerate
+            </button>
+            <button onClick={this.onBreaks} type="button">
+              Apply Brake
+            </button>
           </div>
         </div>
       </div>
